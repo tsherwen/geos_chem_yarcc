458c37694185ad3ab353d6341b57263aa50bfa44
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2017-09-14 12:15:46 +0100

    Update envirn. setup script for v10-1 and v9-2, & add generic settings for SGE script

--- scripts/setup_geos_environment_v10_v9_2.sh
+++ scripts/setup_geos_environment_v10_v9_2.sh
@@ -7,8 +7,7 @@
 module load icc/2013_sp1.3.174
 module load ifort/2013_sp1.3.174
 module load hdf5/1.8.18
-module load NetCDF/4.4.1.1
-module load NetCDF-fortran/4.4.4
+module load NetCDF-complete/4.4.1.1_4.4.4
 
 ################################################################################
 # COMPILER VARIABLES
@@ -30,24 +29,29 @@ export COMPILER=$FC
 #     - icc/2013_sp1.3.174
 #     - ifort/2013_sp1.3.174
 #     - hdf5/1.8.18
-#     - NetCDF/4.4.1.1
-#     - NetCDF-fortran/4.4.4
+#     - NetCDF-complete/4.4.1.1_4.4.4
 # must be loaded in the above order before setting these variables.
 ################################################################################
-GC_C_BASE="/opt/yarcc/libraries/NetCDF/4.4.1.1/1/hdf5-1.8.18-icc-2013_sp1.3.174"
-GC_F_BASE="/opt/yarcc/libraries/NetCDF-fortran/4.4.4/1/\
-hdf5-1.8.18-NetCDF-4.4.1.1-icc-2013_sp1.3.174-ifort-2013_sp1.3.174"
-
-export GC_BIN="$GC_C_BASE/bin"
-export GC_INCLUDE="$GC_C_BASE/include"
-export GC_LIB="$GC_C_BASE/lib"
-export GC_F_BIN="$GC_F_BASE/bin"
-export GC_F_INCLUDE="$GC_F_BASE/include"
-export GC_F_LIB="$GC_F_BASE/lib"
+export GC_BASE="/opt/yarcc/libraries/NetCDF-complete/4.4.1.1_4.4.4/1/icc-2013_sp1.3.174-ifort-2013_sp1.3.174-hdf5-1.8.18"
+export GC_BIN="$GC_BASE/bin"
+export GC_INCLUDE="$GC_BASE/include"
+export GC_LIB="$GC_BASE/lib"
 
 ################################################################################
 # OPENMP VARIABLES
 #-------------------------------------------------------------------------------
 # These must be set before running GEOS-Chem.
 ################################################################################
+# Max out machine limits                                                         
+ulimit -t unlimited              # cputime                                       
+ulimit -f unlimited              # filesize                                      
+ulimit -d unlimited              # datasize                                      
+ulimit -s unlimited              # stacksize                                     
+ulimit -c unlimited              # coredumpsize                                  
+ulimit -m unlimited              # memoryuse                                     
+ulimit -v unlimited              # vmemoryuse                                    
+ulimit -n unlimited              # descriptors                                   
+ulimit -l unlimited              # memorylocked                                  
+ulimit -u unlimited              # maxproc 
+
 export OMP_STACKSIZE=500m
